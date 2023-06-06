ea843eeee6ca8ab8df432adcaafd2c6b3b3d3622
Matheus Mayer <118402543+matheusmayer123@users.noreply.github.com>
2023-06-06 14:35:30 -0300

    Back-end
    Alterações nas ações e retirada do botão atualizar.

---
+++ LojaApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Cat.Models;
+
+
+namespace Cat.Data{
+
+    public class DataContext : DbContext{
+        public DataContext(DbContextOptions<DataContext> options): base(options){
+
+        }
+        public DbSet<Categoria> Categorias {get; set;}
+
+    }
+}
